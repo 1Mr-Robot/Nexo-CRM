74479377e78f2ccf3baf4915c653d0b736474712
Uziel Flores <uzielo8017518@gmail.com>
2026-03-21 14:50:41 -0600

    Cambio de nombre al proyecto

--- Nexo-CRM/wsgi.py
+++ Nexo-CRM/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ProyectoTPOO project.
+WSGI config for Nexo-CRM project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoBD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nexo-CRM.settings')
 
 application = get_wsgi_application()
